414ff9e95491a898a40cbbb6a532c30852bb0892
unknown <rajankarmakar@hotmail.comm>
2019-08-29 20:03:14 +0600

    Updates

---
+++ precache-manifest.f16812ce606da66d958e2a814f86316f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e62f55fb10c2ebd7b78f1fb129394f",
+    "url": "/form/index.html"
+  },
+  {
+    "revision": "140d6b59b269d4e10308",
+    "url": "/form/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "bd8fdd0d1bb86fcd1fc5",
+    "url": "/form/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "140d6b59b269d4e10308",
+    "url": "/form/static/js/2.8122c51d.chunk.js"
+  },
+  {
+    "revision": "bd8fdd0d1bb86fcd1fc5",
+    "url": "/form/static/js/main.75dbc1c6.chunk.js"
+  },
+  {
+    "revision": "fffcf8397d887c1bacfe",
+    "url": "/form/static/js/runtime~main.00df4fb8.js"
+  }
+]);
